4e80e24485107cb92ed94f9d00c7cb91364eb933
Raju <rajutamang3456@gmail.com>
2025-01-21 16:52:03 +0545

    js start

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// confirm("are you okay?");
+// let answer = confirm("are you okay?");
+// console.log("are you oaky?", answer);
+// if (answer) {
+//   console.log("now you are friend with xyz");
+// } else {
+//   console.log("now you are not friend with xyz");
+// }
+// alert("virus detected");
+const number = prompt("enter your age");
+console.log("your age is", number);
---
+++ js/variables.js
@@ -0,0 +1,11 @@
+// var let const
+// var x = 10;
+// var x = 20;
+// console.log("value of x is: ", x);
+// let x = 10;
+// let y = 20;
+// console.log("value of x is ", x);
+
+const x = 10;
+const x = 20;
+console.log("value of x is ", x);
