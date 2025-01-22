2cca4a953ca08f133533e65ab05dcd497cbc4931
Raju <rajutamang3456@gmail.com>
2025-01-22 16:52:38 +0545

    varaibles

--- js/variables.js
+++ js/variables.js
@@ -1,11 +1,21 @@
 // var let const
 // var x = 10;
 // var x = 20;
+// var x = 30;
+// var x = 15;
 // console.log("value of x is: ", x);
 // let x = 10;
 // let y = 20;
 // console.log("value of x is ", x);
 
-const x = 10;
+// const x = 10;
+// const y = 20;
+// console.log("value of x is ", x);
 const x = 20;
-console.log("value of x is ", x);
+
+{
+  const x = 15;
+  console.log("this is block", x);
+  console.log("this is block last", x);
+}
+console.log("this is bottom", x);
