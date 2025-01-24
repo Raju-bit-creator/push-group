074c4d042b6ae90f3a88e64e8c56b80dad92b8c9
Raju <rajutamang3456@gmail.com>
2025-01-24 16:53:41 +0545

    conditions

---
+++ js/conditions.js
@@ -0,0 +1,18 @@
+let age = 17;
+let age1 = 15;
+let name = "Jhon";
+let lastName = "Doe";
+// if (age >= 18) {
+//   console.log("you can vote");
+// } else if (age > 14) {
+//   console.log("you have to wait 2 years to vote");
+// } else {
+//   console.log("you cannot vote");
+// }
+const hari = age >= 18 ? "you can vote" : "you cannot vote";
+console.log(hari);
+
+//assignment
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// make greeting desisions according to days using if , else if and else statement
--- js/operator.js
+++ js/operator.js
@@ -50,15 +50,41 @@
 //   console.log("give me cold drinks");
 // }
 
-let a = 4;
-let b = 10;
-let c = 20;
-if (a > b) {
-  console.log("a is greater than b");
-} else if (a < c) {
-  console.log("a is greater than c");
-} else if (a < c) {
-  console.log("a is greater than c");
+// let a = 4;
+// let b = 10;
+// let c = 20;
+// if (a > b) {
+//   console.log("a is greater than b");
+// } else if (a < c) {
+//   console.log("a is greater than c");
+// } else if (a < c) {
+//   console.log("a is greater than c");
+// } else {
+//   console.log("b is greater than a");
+// }
+let willRain = false;
+let willNotRain = true;
+let scorchingSun = false;
+let probability = true;
+let isCold = false;
+if (willRain) {
+  console.log("take umbrella");
+} else if (probability) {
+  console.log("think your self and take decision");
 } else {
-  console.log("b is greater than a");
+  console.log("no need to take umbrella");
 }
+
+// let a = 20;
+// let b = 30;
+// console.log("sum of a and b is :", a - b);
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log("this is sum", sum(5, 10));
+
+function sum(a, b) {
+  return a - b;
+}
+console.log("this is sum", sum(5, 10));
