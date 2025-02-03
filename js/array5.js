b1444ed0086935ce222a977d9a6f48733434a174
Raju <rajutamang3456@gmail.com>
2025-02-03 16:56:35 +0545

    array5

---
+++ js/array5.js
@@ -0,0 +1,22 @@
+// const sentence = "we/are/happy /to /see /you";
+// const words = sentence.split("/");
+// console.log("words", words);
+// const totalWord = words.length;
+// console.log("total word", totalWord);
+
+let num = [1, 2, 3, 4, 6, 7, 8];
+let newNum = num.push(9);
+console.log(num);
+
+let removeNum = num.pop();
+console.log(num);
+
+// let num2 = [1, 2, 3, 4];
+// let num3 = [...num2, 5, 6, 7]; //spread operator
+// console.log(num3);
+// assignment: to research on shift unshift indexof includes splice slice  methods in array
+
+// json parse // json stringify
+
+let student = { name: "jhone", age: 30 };
+console.log(student.age);
